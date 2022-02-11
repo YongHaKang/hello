66cb2f0a5a2b04ebceac9b343dcfdcccc799a22d
yokang35 <yokang35@coupang.com>
2022-02-11 11:53:23 +0900

    hi

--- src/main/java/com/coupang/tb3/hello/HelloController.java
+++ src/main/java/com/coupang/tb3/hello/HelloController.java
@@ -13,6 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello";
+        return "Hello hi";
     }
 }
